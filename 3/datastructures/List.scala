eccf18760f2dfb06dcc06202561577a16a2c62c2
YU81 <usui1118@yu81.jp>
2016-04-28 16:56:46 +0900

    Exercise 3.22 answer.I could not implemented...

--- 3/datastructures/List.scala
+++ 3/datastructures/List.scala
@@ -337,6 +337,12 @@ The other implementations build up a chain of functions which, when called, resu
   }
 
   def filterViaFlatMap[A](as: List[A])(f: A => Boolean): List[A] = {
-    flatMapAnswer(as)((h) => if (f(h)) List(h) else Nil:List[A])
+    flatMapAnswer(as)((h) => if (f(h)) List(h) else Nil: List[A])
+  }
+
+  def addPairwiseAnswer(a: List[Int], b: List[Int]): List[Int] = (a, b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addPairwiseAnswer(t1, t2))
   }
 }
