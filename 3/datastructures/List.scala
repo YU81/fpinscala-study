bbf90f22c9a53dff84ae39473b6fee55cb81466c
YU81 <usui1118@yu81.jp>
2016-04-28 17:37:42 +0900

    Exercise 3.23

--- 3/datastructures/List.scala
+++ 3/datastructures/List.scala
@@ -345,4 +345,10 @@ The other implementations build up a chain of functions which, when called, resu
     case (_, Nil) => Nil
     case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addPairwiseAnswer(t1, t2))
   }
+
+  def zipWith[A](a: List[A], b: List[A])(f: (A, A) => A): List[A] = (a, b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
+  }
 }
