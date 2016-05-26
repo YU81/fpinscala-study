49e68cc36f9164ae190336e69776debcd4138caf
YU81 <usui1118@yu81.jp>
2016-05-26 17:05:04 +0900

    Exercise 3.28(Tree.map)

--- 3/datastructures/Tree.scala
+++ 3/datastructures/Tree.scala
@@ -31,3 +31,10 @@ object Tree {
     go(t, 1)
   }
 }
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = {
+    t match {
+      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+      case Leaf(v) => Leaf(f(v))
+    }
+  }
+
